267196bc423f13373d067b4115f5a86e27ad8450
martinomburajr <martin.omburajr@gmail.com>
2021-04-25 20:46:36 +0200

    finished first draft of Rust Generics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
-import '../styles.css'
-import 'nextra-theme-docs/style.css'
+import "../styles.css";
+import "nextra-theme-docs/style.css";
 
-import Prism from 'prism-react-renderer/prism'
-(typeof global !== "undefined" ? global : window).Prism = Prism
+import Prism from "prism-react-renderer/prism";
+(typeof global !== "undefined" ? global : window).Prism = Prism;
 
-require("prismjs/components/prism-rust")
-require("prismjs/components/prism-nasm")
-require("prismjs/components/prism-bash")
+require("prismjs/components/prism-rust");
+require("prismjs/components/prism-nasm");
+require("prismjs/components/prism-bash");
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
